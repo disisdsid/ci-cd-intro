50a2038055f93ff46ed053564b87848dfb412815
disisdsid <136875283+disisdsid@users.noreply.github.com>
2024-01-08 21:18:24 +0530

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/user', function (req, res, next) {
+router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
 
 module.exports = router
-#hello
+
